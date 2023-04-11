f8ddc6376e1314894f0de214ac2ba6462c14af70
unknown <terry.grimoire@gmail.com>
2023-04-11 17:25:08 +0400

    ok

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import papa from "papaparse";
 
+import plat from "../assets/plat.png";
+import dessert from "../assets/dessert.png";
+import boisson from "../assets/boisson.png";
+
 function Services({ helmet }) {
   const [pizzas, setPizzas] = useState([]);
   const prepareData = (data) => {
@@ -38,6 +42,8 @@ function Services({ helmet }) {
     );
   };
 
+  const [choice, setChoice] = useState("plat");
+
   useEffect(() => {
     window.scrollTo(0, 0);
 
@@ -55,23 +61,67 @@ function Services({ helmet }) {
       </Helmet>
       <section>
         <h1>Notre carte</h1>
+        <div className="button_container">
+          <button
+            type="button"
+            className={
+              choice === "plat" ? "button_Choice choisie" : "button_Choice"
+            }
+            onClick={() => setChoice("plat")}
+          >
+            <img src={plat} alt="pizza icone" />
+            <p>Les plats</p>
+          </button>
+          <button
+            type="button"
+            className={
+              choice === "dessert" ? "button_Choice choisie" : "button_Choice"
+            }
+            onClick={() => setChoice("dessert")}
+          >
+            <img src={dessert} alt="poivron icone" />
+            <p>Desserts</p>
+          </button>
+          <button
+            type="button"
+            className={
+              choice === "boisson" ? "button_Choice choisie" : "button_Choice"
+            }
+            onClick={() => setChoice("boisson")}
+          >
+            <img src={boisson} alt="cupcake icone" />
+            <p>Boissons</p>
+          </button>
+        </div>
 
         <div>
           <p className="dispo">
             {pizzas !== null &&
-            pizzas.filter((el) => el.Nom !== "Nom").length > 0 ? (
+            pizzas.filter((el) => el.Nom !== "Nom" && el.Type.includes(choice))
+              .length > 0 ? (
               <>
-                {pizzas.filter((el) => el.Nom !== "Nom").length} résultats
-                disponibles
+                {
+                  pizzas.filter(
+                    (el) => el.Nom !== "Nom" && el.Type.includes(choice)
+                  ).length
+                }{" "}
+                résultats disponibles
               </>
             ) : (
               <p>chargement de la carte</p>
             )}
           </p>
+          <div className="dispo">
+            {choice === "plat" &&
+              pizzas.filter(
+                (el) => el.Nom !== "Nom" && el.Type.includes(choice)
+              ).length > 0 &&
+              pizzas !== null && <h3>Nos plats du jour</h3>}
+          </div>
           <div>
             {pizzas !== null &&
               pizzas
-                .filter((el) => el.Nom !== "Nom")
+                .filter((el) => el.Nom !== "Nom" && el.Type.includes(choice))
                 .map((pizza) => (
                   <div className="plats" key={pizza.Nom}>
                     <div>
